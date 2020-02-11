fcd417b64e2593019a6269ef939726b30414f87b
John Chabot <johnchabot2013@gmail.com>
2020-02-10 20:19:18 -0500

    Add more integration tests

--- tests/integrations.rs
+++ tests/integrations.rs
@@ -33,6 +33,8 @@ fn fixture_path(path: &str) -> Option<PathBuf> {
 fn test_files(paths: &[&str], result: InterpretResult) -> Result<(), std::io::Error> {
   for path in paths {
     let mut vm = create_vm();
+
+    println!("file: {}", path);
     let assert = fixture_path(path).expect("No parent directory");
 
     let mut file = File::open(assert)?;
@@ -75,7 +77,6 @@ fn assert() -> Result<(), std::io::Error> {
 fn assert_eq() -> Result<(), std::io::Error> {
   let mut vm = create_vm();
   let assert = fixture_path("assert/assert_eq.lox").expect("No parent directory");
-  // assert_eq!(assert, Path::new(""));
 
   let mut file = File::open(assert)?;
   let mut source = String::new();
@@ -89,7 +90,6 @@ fn assert_eq() -> Result<(), std::io::Error> {
 fn assert_ne() -> Result<(), std::io::Error> {
   let mut vm = create_vm();
   let assert = fixture_path("assert/assert_ne.lox").expect("No parent directory");
-  // assert_eq!(assert, Path::new(""));
 
   let mut file = File::open(assert)?;
   let mut source = String::new();
@@ -169,17 +169,31 @@ fn call() -> Result<(), std::io::Error> {
 //   ], InterpretResult::RuntimeError)
 // }
 
-// #[test]
-// fn closure() -> Result<(), std::io::Error> {
-//   test_files(&vec![
-//   ], InterpretResult::Ok)?;
+#[test]
+fn closure() -> Result<(), std::io::Error> {
+  test_files(
+    &vec![
+      "closure/assign_to_closure.lox",
+      "closure/assign_to_shadowed_later.lox",
+      "closure/close_over_function_parameter.lox",
+      "closure/close_over_later_variable.lox",
+      // "closure/close_over_method_parameter.lox",
+      "closure/closed_closure_in_function.lox",
+      "closure/nested_closure.lox",
+      "closure/open_closure_in_function.lox",
+      "closure/reference_closure_multiple_times.lox",
+      "closure/reuse_closure_slot.lox",
+      "closure/shadow_closure_with_local.lox",
+      "closure/unused_closure.lox",
+      "closure/unused_later_closure.lox",
+    ],
+    InterpretResult::Ok,
+  )?;
 
-//   test_files(&vec![
-//   ], InterpretResult::CompileError)?;
+  test_files(&vec![], InterpretResult::CompileError)?;
 
-//   test_files(&vec![
-//   ], InterpretResult::RuntimeError)
-// }
+  test_files(&vec![], InterpretResult::RuntimeError)
+}
 
 #[test]
 fn comments() -> Result<(), std::io::Error> {
@@ -235,6 +249,7 @@ fn for_loop() -> Result<(), std::io::Error> {
       "for/return_inside.lox",
       "for/scope.lox",
       "for/syntax.lox",
+      "for/closure_in_body.lox",
     ],
     InterpretResult::Ok,
   )?;
@@ -242,7 +257,6 @@ fn for_loop() -> Result<(), std::io::Error> {
   test_files(
     &vec![
       // "for/class_in_body.lox"
-      // "for/closure_in_body.lox"
       "for/fun_in_body.lox",
       "for/statement_condition.lox",
       "for/statement_increment.lox",
@@ -255,13 +269,12 @@ fn for_loop() -> Result<(), std::io::Error> {
   test_files(&vec![], InterpretResult::RuntimeError)
 }
 
-
 #[test]
 fn function() -> Result<(), std::io::Error> {
   test_files(
     &vec![
       "function/empty_body.lox",
-      // "function/local_recursion.lox",
+      "function/local_recursion.lox",
       "function/mutual_recursion.lox",
       "function/parameters.lox",
       "function/print.lox",
@@ -280,11 +293,14 @@ fn function() -> Result<(), std::io::Error> {
     InterpretResult::CompileError,
   )?;
 
-  test_files(&vec![
-    "function/extra_arguments.lox",
-    "function/local_mutual_recursion.lox",
-    "function/missing_arguments.lox",
-  ], InterpretResult::RuntimeError)
+  test_files(
+    &vec![
+      "function/extra_arguments.lox",
+      "function/local_mutual_recursion.lox",
+      "function/missing_arguments.lox",
+    ],
+    InterpretResult::RuntimeError,
+  )
 }
 
 #[test]
@@ -311,8 +327,7 @@ fn if_stmt() -> Result<(), std::io::Error> {
     InterpretResult::CompileError,
   )?;
 
-  test_files(&vec![
-  ], InterpretResult::RuntimeError)
+  test_files(&vec![], InterpretResult::RuntimeError)
 }
 
 // #[test]
@@ -330,40 +345,43 @@ fn if_stmt() -> Result<(), std::io::Error> {
 
 #[test]
 fn limit() -> Result<(), std::io::Error> {
-  test_files(&vec![
-    "expressions/evaluate.lox"
-  ], InterpretResult::Ok)?;
+  test_files(&vec!["expressions/evaluate.lox"], InterpretResult::Ok)?;
 
-  test_files(&vec![
-    "limit/loop_too_large.lox",
-    "limit/no_reuse_constants.lox",
-    "limit/too_many_constants.lox",
-    "limit/too_many_locals.lox",
-    
-    // "limit/too_many_upvalues.lox",
-  ], InterpretResult::CompileError)?;
+  test_files(
+    &vec![
+      "limit/loop_too_large.lox",
+      "limit/no_reuse_constants.lox",
+      "limit/too_many_constants.lox",
+      "limit/too_many_locals.lox",
+      "limit/too_many_upvalues.lox",
+    ],
+    InterpretResult::CompileError,
+  )?;
 
-  test_files(&vec![
-    // "limit/stack_overflow.lox"
-  ], InterpretResult::RuntimeError)
+  test_files(
+    &vec![
+      // "limit/stack_overflow.lox"
+    ],
+    InterpretResult::RuntimeError,
+  )
 }
 
 #[test]
 fn logical_operator() -> Result<(), std::io::Error> {
-  test_files(&vec![
-    "logical_operator/and_truth.lox",
-    // "logical_operator/and.lox",
-    "logical_operator/or_truth.lox",
-    "logical_operator/or.lox",
-  ], InterpretResult::Ok)?;
+  test_files(
+    &vec![
+      "logical_operator/and_truth.lox",
+      "logical_operator/and.lox",
+      "logical_operator/or_truth.lox",
+      "logical_operator/or.lox",
+    ],
+    InterpretResult::Ok,
+  )?;
 
-  test_files(&vec![
-  ], InterpretResult::CompileError)?;
+  test_files(&vec![], InterpretResult::CompileError)?;
 
-  test_files(&vec![
-  ], InterpretResult::RuntimeError)
+  test_files(&vec![], InterpretResult::RuntimeError)
 }
-
 
 // #[test]
 // fn method() -> Result<(), std::io::Error> {
@@ -379,45 +397,122 @@ fn logical_operator() -> Result<(), std::io::Error> {
 
 #[test]
 fn nil() -> Result<(), std::io::Error> {
-  test_files(&vec![
-    "nil/literal.lox"
-  ], InterpretResult::Ok)?;
+  test_files(&vec!["nil/literal.lox"], InterpretResult::Ok)?;
 
-  test_files(&vec![
-  ], InterpretResult::CompileError)?;
+  test_files(&vec![], InterpretResult::CompileError)?;
 
-  test_files(&vec![
-  ], InterpretResult::RuntimeError)
+  test_files(&vec![], InterpretResult::RuntimeError)
 }
-
 
 #[test]
 fn number() -> Result<(), std::io::Error> {
-  test_files(&vec![
-    "number/literals.lox",
-  ], InterpretResult::Ok)?;
+  test_files(&vec!["number/literals.lox"], InterpretResult::Ok)?;
 
-  test_files(&vec![
-    "number/decimal_point_at_eof.lox",
-    "number/leading_dot.lox",
-    "number/trailing_dot.lox",
-  ], InterpretResult::CompileError)?;
+  test_files(
+    &vec![
+      "number/decimal_point_at_eof.lox",
+      "number/leading_dot.lox",
+      "number/trailing_dot.lox",
+    ],
+    InterpretResult::CompileError,
+  )?;
 
-  test_files(&vec![
-  ], InterpretResult::RuntimeError)
+  test_files(&vec![], InterpretResult::RuntimeError)
+}
+
+#[test]
+fn operator() -> Result<(), std::io::Error> {
+  test_files(
+    &vec![
+      "operator/add.lox",
+      "operator/comparison.lox",
+      "operator/divide.lox",
+      // "operator/equals_class.lox",
+      // "operator/equals_method.lox",
+      "operator/equals.lox",
+      "operator/multiply.lox",
+      "operator/negate.lox",
+      // "operator/not_class.lox",
+      "operator/not_equals.lox",
+      "operator/not.lox",
+      "operator/subtract.lox",
+    ],
+    InterpretResult::Ok,
+  )?;
+
+  test_files(&vec![], InterpretResult::CompileError)?;
+
+  test_files(
+    &vec![
+      "operator/add_bool_nil.lox",
+      "operator/add_bool_num.lox",
+      "operator/add_bool_string.lox",
+      "operator/add_nil_nil.lox",
+      "operator/add_num_nil.lox",
+      "operator/add_string_nil.lox",
+      "operator/divide_nonnum_num.lox",
+      "operator/divide_num_nonnum.lox",
+      "operator/greater_nonnum_num.lox",
+      "operator/greater_num_nonnum.lox",
+      "operator/greater_or_equal_nonnum_num.lox",
+      "operator/greater_or_equal_num_nonnum.lox",
+      "operator/less_nonnum_num.lox",
+      "operator/less_num_nonnum.lox",
+      "operator/less_or_equal_nonnum_num.lox",
+      "operator/less_or_equal_num_nonnum.lox",
+      "operator/multiply_nonnum_num.lox",
+      "operator/multiply_num_nonnum.lox",
+      "operator/negate_nonnum.lox",
+      "operator/subtract_nonnum_num.lox",
+      "operator/subtract_num_nonnum.lox",
+    ],
+    InterpretResult::RuntimeError,
+  )
 }
 
 #[test]
 fn print() -> Result<(), std::io::Error> {
-  test_files(&vec![
-  ], InterpretResult::Ok)?;
+  test_files(&vec![], InterpretResult::Ok)?;
 
-  test_files(&vec![
-    "print/missing_argument.lox",
-  ], InterpretResult::CompileError)?;
+  test_files(
+    &vec!["print/missing_argument.lox"],
+    InterpretResult::CompileError,
+  )?;
 
-  test_files(&vec![
-  ], InterpretResult::RuntimeError)
+  test_files(&vec![], InterpretResult::RuntimeError)
+}
+
+#[test]
+fn regression() -> Result<(), std::io::Error> {
+  test_files(
+    &vec![
+      "regression/40.lox",
+      // "regression/394.lox"
+    ],
+    InterpretResult::Ok,
+  )
+}
+
+#[test]
+fn return_test() -> Result<(), std::io::Error> {
+  test_files(
+    &vec![
+      "return/after_else.lox",
+      "return/after_if.lox",
+      "return/after_while.lox",
+      "return/in_function.lox",
+      "return/return_nil_if_no_value.lox",
+      "return/return_nil_if_no_value.lox",
+    ],
+    InterpretResult::Ok,
+  )?;
+
+  test_files(
+    &vec!["return/at_top_level.lox"],
+    InterpretResult::CompileError,
+  )?;
+
+  test_files(&vec![], InterpretResult::RuntimeError)
 }
 
 // #[test]
@@ -432,17 +527,44 @@ fn print() -> Result<(), std::io::Error> {
 //   ], InterpretResult::RuntimeError)
 // }
 
-
-// #[test]
+#[test]
 fn string() -> Result<(), std::io::Error> {
-  test_files(&vec![
-  ], InterpretResult::Ok)?;
+  test_files(
+    &vec!["string/literals.lox", "string/multiline.lox"],
+    InterpretResult::Ok,
+  )?;
 
-  test_files(&vec![
-    "print/missing_argument.lox",
-  ], InterpretResult::CompileError)?;
+  test_files(
+    &vec!["string/unterminated.lox"],
+    InterpretResult::CompileError,
+  )?;
 
-  test_files(&vec![
-  ], InterpretResult::RuntimeError)
+  test_files(
+    &vec!["string/error_after_multiline.lox"],
+    InterpretResult::RuntimeError,
+  )
 }
 
+#[test]
+fn while_test() -> Result<(), std::io::Error> {
+  test_files(
+    &vec![
+      "while/closure_in_body.lox",
+      "while/return_closure.lox",
+      "while/return_inside.lox",
+      "while/syntax.lox",
+    ],
+    InterpretResult::Ok,
+  )?;
+
+  test_files(
+    &vec![
+      // "while/class_in_body.lox",
+      "while/fun_in_body.lox",
+      "while/var_in_body.lox",
+    ],
+    InterpretResult::CompileError,
+  )?;
+
+  test_files(&vec![], InterpretResult::RuntimeError)
+}
