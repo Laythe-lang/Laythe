0992b93678739ca5bbf90391087be6974e294260
John Chabot <johnchabot2013@gmail.com>
2020-05-05 00:18:34 -0400

    Add macro crate. - Add new trace macro when native want old behavior

---
+++ spacelox_macro/src/lib.rs
@@ -0,0 +1,28 @@
+extern crate proc_macro;
+
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Trace)]
+pub fn derive_trace(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+  // Parse the input tokens into a syntax tree.
+  let input = parse_macro_input!(input as DeriveInput);
+
+  // Used in the quasi-quotation below as `#name`.
+  let name = input.ident;
+
+  let expanded = quote! {
+    impl Trace for #name {
+      fn trace(&self) -> bool {
+        true
+      }
+
+      fn trace_debug(&self, _: &dyn StdIo) -> bool {
+        true
+      }
+    }
+  };
+
+  // Hand the output tokens back to the compiler.
+  proc_macro::TokenStream::from(expanded)
+}
