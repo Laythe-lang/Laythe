6e7aecc6e0ec3cf114626c9f805884615d30f185
John Chabot <johnchabot2013@gmail.com>
2020-07-14 19:42:10 -0400

    Add wasm implementation, Test website - Website houses editor and repl - Some wasm specifc io impl

---
+++ laythe_wasm/web/index.js
@@ -0,0 +1,81 @@
+import { VmWasm } from "laythe";
+import { terminalInit, editorInit } from "./init";
+
+const runElement = document.getElementById("run");
+const replElement = document.getElementById("repl");
+
+const editorElement = document.getElementById("editor");
+const terminalElement = document.getElementById("terminal");
+
+let repl = false;
+let curr_line = "";
+
+const editor = editorInit(editorElement);
+const term = terminalInit(terminalElement, VmWasm.version());
+
+let timeoutHandle;
+
+const runScript = () => {
+  repl = false;
+  clearTimeout(timeoutHandle);
+  const [model] = editor.getModels();
+  const source = model.getValue();
+
+  term.clear();
+  const vm = VmWasm.with_stdout((str) => term.writeln(str));
+
+  setImmediate(() => vm.run(source));
+};
+
+const startRepl = () => {
+  if (!repl) {
+    repl = true;
+
+    clearTimeout(timeoutHandle);
+    term.clear();
+    setImmediate(() => term.write('> '));
+  }
+};
+
+timeoutHandle = setTimeout(startRepl, 5000);
+
+// VM for the repl
+const replVm = VmWasm.with_stdout((str) => {
+  term.writeln(str);
+});
+
+// on run strip source from editor and pipe it through a new vm
+runElement.addEventListener("click", runScript);
+
+// on repl clear terminal and set repl to true
+replElement.addEventListener("click", startRepl);
+
+term.onKey(({ key, domEvent: { keyCode } }) => {
+  if (repl) {
+    console.log(key, keyCode);
+
+    switch (keyCode) {
+      case 13:
+        term.write('\r\n');
+        replVm.run(curr_line);
+        term.write('\r\n> ');
+        curr_line = "";
+        break
+      case 8:
+        if (curr_line) {
+          curr_line = curr_line.slice(0, curr_line.length - 1);
+          term.write('\b \b');
+        }
+        break
+      default:
+        curr_line += key;
+        term.write(key);
+    }
+  }
+});
+
+term.attachCustomKeyEventHandler((event) => {
+  console.log(event);
+  return true;
+});
+
