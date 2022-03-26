44f91e2e783f5493027749da243452293071e7ac
John Chabot <johnchabot2013@gmail.com>
2022-03-26 16:15:00 -0400

    pull out impls into module

--- laythe_vm/src/vm/mod.rs
+++ laythe_vm/src/vm/mod.rs
@@ -1,3 +1,5 @@
+mod impls;
+
 use crate::{
   byte_code::{AlignedByteCode, ByteCode, CaptureIndex},
   cache::InlineCache,
@@ -10,15 +12,15 @@ use codespan_reporting::term::{self, Config};
 use laythe_core::{
   captures::Captures,
   constants::{PLACEHOLDER_NAME, SELF},
-  hooks::{GcContext, GcHooks, HookContext, Hooks, NoContext, ValueContext},
+  hooks::{GcContext, GcHooks, HookContext, Hooks, NoContext},
   if_let_obj,
   managed::{
-    Allocate, Array, DebugHeapRef, Gc, GcObj, GcObject, GcStr, Instance, Object, Trace, TraceRoot,
+    Allocate, Array, DebugHeapRef, Gc, GcObj, GcObject, GcStr, Instance, Object, Trace,
     Tuple,
   },
   match_obj,
   memory::Allocator,
-  module::{Import, Module, Package, ImportError},
+  module::{Import, ImportError, Module, Package},
   object::{
     Channel, Class, Closure, Fiber, Fun, List, LyBox, Map, Method, Native, NativeMeta, ObjectKind,
     ReceiveResult, SendResult, UnwindResult,
@@ -37,7 +39,7 @@ use std::mem;
 use std::path::PathBuf;
 use std::ptr;
 use std::{cell::RefCell, cmp::Ordering};
-use std::{collections::VecDeque, io::Write};
+use std::{collections::VecDeque};
 use std::{convert::TryInto, usize};
 
 #[cfg(feature = "debug")]
@@ -1419,8 +1421,11 @@ impl Vm {
       Some(package) => {
         match package
           .import(&GcHooks::new(self), import)
-          .and_then(|module| module.get_exported_symbol(name).ok_or(ImportError::SymbolDoesNotExist))
-        {
+          .and_then(|module| {
+            module
+              .get_exported_symbol(name)
+              .ok_or(ImportError::SymbolDoesNotExist)
+          }) {
           Ok(symbol) => {
             self.fiber.push(val!(symbol));
             Signal::Ok
@@ -2318,82 +2323,4 @@ fn assert_roots(native: GcObj<Native>, roots_before: usize, roots_now: usize) {
     native.meta().name,
     roots_now - roots_before,
   );
-}
-
-impl TraceRoot for Vm {
-  fn trace(&self) {
-    self.fiber.trace();
-    self.main_fiber.trace();
-    for fiber in &self.fiber_queue {
-      fiber.trace();
-    }
-    self.files.trace();
-    self.packages.trace();
-    self.module_cache.trace();
-    self.capture_stub.trace();
-
-    for stub in &self.native_fun_stubs {
-      stub.trace();
-    }
-  }
-
-  fn trace_debug(&self, log: &mut dyn Write) {
-    self.fiber.trace_debug(log);
-    self.main_fiber.trace_debug(log);
-    for fiber in &self.fiber_queue {
-      fiber.trace_debug(log);
-    }
-    self.files.trace_debug(log);
-    self.packages.trace_debug(log);
-    self.module_cache.trace_debug(log);
-    self.capture_stub.trace_debug(log);
-
-    for stub in &self.native_fun_stubs {
-      stub.trace_debug(log);
-    }
-  }
-
-  fn can_collect(&self) -> bool {
-    true
-  }
-}
-
-impl HookContext for Vm {
-  fn gc_context(&self) -> &dyn GcContext {
-    self
-  }
-
-  fn value_context(&mut self) -> &mut dyn ValueContext {
-    self
-  }
-
-  fn io(&mut self) -> Io {
-    self.io.clone()
-  }
-}
-
-impl GcContext for Vm {
-  fn gc(&self) -> std::cell::RefMut<'_, Allocator> {
-    self.gc.borrow_mut()
-  }
-}
-
-impl ValueContext for Vm {
-  fn call(&mut self, callable: Value, args: &[Value]) -> Call {
-    let result = unsafe { self.run_fun(callable, args) };
-    self.to_call_result(result)
-  }
-
-  fn call_method(&mut self, this: Value, method: Value, args: &[Value]) -> Call {
-    let result = unsafe { self.run_method(this, method, args) };
-    self.to_call_result(result)
-  }
-
-  fn get_method(&mut self, this: Value, method_name: GcStr) -> Call {
-    unsafe { self.get_method(this, method_name) }
-  }
-
-  fn get_class(&mut self, this: Value) -> Value {
-    val!(self.value_class(this))
-  }
 }
